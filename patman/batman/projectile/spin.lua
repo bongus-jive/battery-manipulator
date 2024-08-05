06812a50d51882a6ab1f9ab7616efe957bc41c63
patman <27874300+patmanf@users.noreply.github.com>
2024-08-04 21:36:58 -0500

    consistenter/longer variable names for spin

--- patman/batman/projectile/spin.lua
+++ patman/batman/projectile/spin.lua
@@ -1,18 +1,18 @@
-local oi = init
-local ou = update
+local oldInit = init
+local oldUpdate = update
 local rotationSpeed
 
 function init()
-	if oi then oi() end
+	if oldInit then oldInit() end
 	
 	rotationSpeed = config.getParameter("rotationSpeed", 1)
 end
 
 function update(dt)
-	if ou then ou(dt) end
+	if oldUpdate then oldUpdate(dt) end
 	
 	local velocity = mcontroller.velocity()
-	local dir = velocity[1] > 0 and 1 or -1
-	local rotation = (vec2.mag(velocity) / 180 * math.pi) * -dir * dt * rotationSpeed * (self.returning and -1 or 1)
+	local direction = velocity[1] > 0 and 1 or -1
+	local rotation = (vec2.mag(velocity) / 180 * math.pi) * dt * -direction * rotationSpeed * (self.returning and -1 or 1)
 	mcontroller.setRotation(mcontroller.rotation() + rotation)
 end
